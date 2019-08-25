1b23108d8a5716d3d7b70e427a105a9972cea2f9
PabloLMartinez2 <pablo.martinez@sigsensetech.com>
2019-08-25 17:09:46 -0300

    remove mobile styles from login

--- src/auth/components/LoginComponent.js
+++ src/auth/components/LoginComponent.js
@@ -8,7 +8,6 @@ class LoginComponent extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            width: window.innerWidth,
             isAuthenticated: true
         };
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,8 +22,7 @@ class LoginComponent extends Component{
     }
 
     render() {
-        const { width, isAuthenticated } = this.state;
-        const isMobile = width <= 500;
+        const { isAuthenticated } = this.state;
 
         const errorMessage = isAuthenticated ? (
             <span>Enter your details below.</span>
@@ -32,61 +30,33 @@ class LoginComponent extends Component{
             <span className="errorMessage">Oops! That email and password combination is not valid.</span>
         );
 
-        if (isMobile) {
-            return (
-                <div className="login-wrapper-mobile">
-                    <div className="logo-mobile" />
-                    <div className="login-form">
-                        <h2>Sign in to Eventio.</h2>
-                        {errorMessage}
-                        <form onSubmit={this.handleSubmit}>
-                            <div className={isAuthenticated ? "wrap-input" : "wrap-input-error" }>
-                                <input type="text" placeholder="Email" ref={(email) => this.email = email}/>
-                            </div>
-                            <div className={isAuthenticated ? "wrap-input" : "wrap-input-error" }>
-                                <input type="password" placeholder="Password"
-                                       ref={(password) => this.password = password}/>
-                                <span className="btn-show-pass">
-                                    <i className="fas fa-eye"/>
-                                </span>
-                            </div>
-                            <div className="sign-up">
-                                <span>Don't have account?</span><a href="#">SIGN UP</a>
-                            </div>
-                            <button className="sign-in-button"> SIGN IN</button>
-                        </form>
-                    </div>
+        return (
+            <div className="login-wrapper-desktop">
+                <div className="login-slide">
+                    <Slide/>
                 </div>
-            );
-        } else {
-            return (
-                <div className="login-wrapper-desktop">
-                    <div className="login-slide">
-                        <Slide/>
-                    </div>
-                    <div className="login-form">
-                        <h2>Sign in to Eventio.</h2>
-                        {errorMessage}
-                        <form onSubmit={this.handleSubmit}>
-                            <div className={isAuthenticated ? "wrap-input" : "wrap-input-error" }>
-                                <input type="text" placeholder="Email" ref={(email) => this.email = email}/>
-                            </div>
-                            <div className={isAuthenticated ? "wrap-input" : "wrap-input-error" }>
-                                <input type="password" placeholder="Password"
-                                       ref={(password) => this.password = password}/>
-                                <span className="btn-show-pass">
-                                    <i className="fas fa-eye"/>
-                                </span>
-                            </div>
-                            <button className="sign-in-button"> SIGN IN</button>
-                        </form>
-                    </div>
-                    <div className="sign-up">
-                        <span>Don't have account?</span><a href="#">SIGN UP</a>
-                    </div>
+                <div className="login-form">
+                    <h2>Sign in to Eventio.</h2>
+                    {errorMessage}
+                    <form onSubmit={this.handleSubmit}>
+                        <div className={isAuthenticated ? "wrap-input" : "wrap-input-error" }>
+                            <input type="text" placeholder="Email" ref={(email) => this.email = email}/>
+                        </div>
+                        <div className={isAuthenticated ? "wrap-input" : "wrap-input-error" }>
+                            <input type="password" placeholder="Password"
+                                   ref={(password) => this.password = password}/>
+                            <span className="btn-show-pass">
+                                <i className="fas fa-eye"/>
+                            </span>
+                        </div>
+                        <button className="sign-in-button"> SIGN IN</button>
+                    </form>
                 </div>
-            );
-        }
+                <div className="sign-up">
+                    <span>Don't have account?</span><a href="#">SIGN UP</a>
+                </div>
+            </div>
+        );
     }
 }
 
